80811f5bd7fa607fb94d6ec71af9eb11244b0194
masaru-yamazaki <masaru.yamazaki@fox-hound.tech>
2019-10-21 01:46:07 +0900

    SPA対応、静的ファイル配信をSpringBootのみで完結するように調整。まだapiは叩いていません。(※srcをほぼ自動生成できるようにしたので本当はsrc内をコミット対象に含めないようにするべき…/確認用にセキュリティフィルターをコメントアウトしています。)

--- TimeCardWeb/src/main/java/com/example/config/WebSecurityConfig.java
+++ TimeCardWeb/src/main/java/com/example/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.ImportResource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
 @ImportResource("classpath:META-INF/spring/applicationContext.xml")
@@ -14,5 +15,18 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter{
 		// 2.ドメイン(MapperBean)などの設定はこちら → KishaKadaiTimeCard-infra.xml
 		// 3.セキュリティ関連はこちら → spring-security.xml
 		// 4.アプリケーションコンテキスト(上記1～3やproperty等を読み込み) → applicationContext.xml
+
+        http
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // No session will be created or used by spring security
+        .and()
+            .httpBasic()
+        .and()
+            .authorizeRequests()
+                .antMatchers("/api/hello").permitAll()
+                .antMatchers("/api/user/**").permitAll() // allow every URI, that begins with '/api/user/'
+                .antMatchers("/api/secured").authenticated()
+                //.anyRequest().authenticated() // protect all other requests
+        .and()
+            .csrf().disable(); // disable cross site request forgery, as we don't use cookies - otherwise ALL PUT, POST, DELETE will get HTTP 403!
 	}
 }
--- TimeCardWeb/src/main/java/com/example/controller/LoginController.java
+++ TimeCardWeb/src/main/java/com/example/controller/LoginController.java
@@ -2,17 +2,27 @@ package com.example.controller;
 
 import static com.example.common.PathConst.*;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping(LOGIN_PAGE)
+//@Controller
+////LOGIN_PAGE
+//@RequestMapping("/")
+@RestController()
+@RequestMapping("/api")
 public class LoginController {
 
 	public static final String HTML_PATH = LOGIN_PAGE;
-    @RequestMapping
-    public String index(Model model) {
-        return HTML_PATH;
+//    @RequestMapping
+//    public String index(Model model) {
+//        return "/index";
+//    }
+
+
+    // Forwards all routes to FrontEnd except: '/', '/index.html', '/api', '/api/**'
+    // Required because of 'mode: history' usage in frontend routing, see README for further details
+    @RequestMapping(value = "{_:^(?!index\\.html|api).$}")
+    public String redirectApi() {
+        return "forward:/";
     }
 }
